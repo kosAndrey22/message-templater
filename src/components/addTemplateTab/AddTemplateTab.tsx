f79b93c17293b456f5f41dfeba1309ff50d0b58e
kosAndrey22 <kosoban22@gmail.com>
2023-03-14 02:08:48 +0400

    Add info tooltip

--- src/components/addTemplateTab/AddTemplateTab.tsx
+++ src/components/addTemplateTab/AddTemplateTab.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, useState } from 'react';
+import { usePopperTooltip } from 'react-popper-tooltip';
 import { saveNewTemplate } from '../../helpers';
 import './AddTemplateTab.scss';
 
@@ -14,19 +15,45 @@ export const AddTemplateTab = (): JSX.Element => {
     setTemplateText('');
   };
 
+  const {
+    getArrowProps,
+    getTooltipProps,
+    setTooltipRef,
+    setTriggerRef,
+    visible,
+  } = usePopperTooltip();
+
   return (
-    <div className='new-template-container'>
-      <div className='form'>
-        <textarea
-          value={templateText}
-          onChange={(e): void => onInputChange(e)}
-        />
-        <button
-          onClick={(): void => onSave()}
-        >
-          Save
-        </button>
+    <>
+      <div className='info-tooltip-mark-container'>
+        <p className='info-tooltip-mark' ref={setTriggerRef} />
+        {visible && (
+          <div
+            ref={setTooltipRef}
+            {...getTooltipProps({ className: 'tooltip-container' })}
+          >
+            <div {...getArrowProps({ className: 'tooltip-arrow' })} />
+            <div className='info-tooltip'>
+              <h4> Available variables: </h4>
+              <p> {'{firstName} - first name of the user'} </p>
+              <p> {'{lastName} - last name of the user'} </p>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+      <div className='new-template-container'>
+        <div className='form'>
+          <textarea
+            value={templateText}
+            onChange={(e): void => onInputChange(e)}
+          />
+          <button
+            onClick={(): void => onSave()}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
